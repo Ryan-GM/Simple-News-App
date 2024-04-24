4a21c8dc0d2c47f697d1534b3e9a25757f7b062e
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-04-24 14:14:36 +0300

    adding function call to display the news

---
+++ script.js
@@ -0,0 +1,13 @@
+const apiKey = process.env.NEWS_API_KEY;
+const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+
+async function fetchNews(){
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        showNews(data);
+    } catch(error){
+        console.error('There was an error!', error);
+    }
+}
